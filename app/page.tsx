04f810cd4d041411a334a13f48ecd78ffa8b1d36
MJ1717 <bananahamo77@gmail.com>
2026-04-02 17:38:40 -0400

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { supabase } from "@/lib/supabase";
 import QueueForm from "./components/QueueForm";
 import QueueRow from "./components/QueueRow";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const { data: queue } = await supabase
     .from("queue_entries")
